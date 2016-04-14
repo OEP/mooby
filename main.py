662288430ba0fe8af72ca5a116f9fa4dc0f98123
Paul Kilgo <paulkilgo@gmail.com>
2016-04-13 20:46:33 -0400

    Change some parameters via command line

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import mooby
-import sys
+import argparse
 
-brain = mooby.Brain(order=1)
 
-for corpus in sys.argv[1:]:
-    brain.learn(open(corpus).read())
-for _ in range(5):
-    print(brain.speak())
+def main():
+    parser = _get_parser()
+    args = parser.parse_args()
+    brain = mooby.Brain(order=args.order)
+
+    for corpus in args.corpus:
+        brain.learn(open(corpus).read())
+
+    for _ in range(args.count):
+        print(brain.speak())
+
+
+def _get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('corpus', nargs='+')
+    parser.add_argument('-k', '--order', type=int, default=1)
+    parser.add_argument('-n', '--count', type=int, default=1)
+    return parser
+
+if __name__ == '__main__':
+    main()
